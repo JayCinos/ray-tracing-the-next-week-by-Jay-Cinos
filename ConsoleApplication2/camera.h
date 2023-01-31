6fa4536be8002d42a6fafe8ebdd2aad870465a06
jay <jaypats@163.com>
2023-02-01 00:38:51 +0800

    Positioning and Orienting the Camera

--- ConsoleApplication2/camera.h
+++ ConsoleApplication2/camera.h
@@ -4,22 +4,29 @@
 #include "vec3.h"
 class camera {
 public:
-	camera(double fov, double aspect_ratio){
+	camera(point3 pointfrom, 
+			point3 pointat,
+			vec3 cam_up,
+			double fov, double aspect_ratio){
 		
 		auto theta = degrees_to_radians(fov);
 		auto h = tan(theta/2 );
 		auto view_height = 2 * h;
 		auto view_width = aspect_ratio * view_height;
-		auto focal_length = 1.0;
-		
-		origin = point3(0, 0, 0);
-		horizion = vec3(view_width, 0, 0);
-		vertical = vec3(0, view_height, 0);
-		lower_left_cornor = origin - horizion/2 - vertical/2 - vec3(0, 0, focal_length);
+
+		auto w = unit_vector(pointfrom -  pointat);
+		auto u = unit_vector(cross(cam_up, w));
+		auto v = cross(w, u);
+
+		horizion = view_width * u;
+		vertical = view_height * v;
+
+		origin = pointfrom;
+		lower_left_cornor = origin - horizion / 2 - vertical / 2  -  w;
 
 	}
-	ray get_ray(double u, const double v) const{
-			return ray(origin, lower_left_cornor + u*horizion + v*vertical - origin);
+	ray get_ray(double u_1, const double v_1) const{
+			return ray(origin, lower_left_cornor + u_1 * horizion  + v_1 * vertical - origin);
 		}
 public:
 	point3 origin;
